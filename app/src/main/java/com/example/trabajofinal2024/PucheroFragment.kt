9ca1ecc580285f38123b813b3858804ab51b7be1
Irigoyen Carlos Damian <cyrigoyen969@gmail.com>
2024-06-22 21:14:50 -0300

    se completo la parte de platos populares

---
+++ app/src/main/java/com/example/trabajofinal2024/PucheroFragment.kt
@@ -0,0 +1,170 @@
+package com.example.trabajofinal2024
+import android.os.Bundle
+import android.text.Editable
+import android.text.InputFilter
+import android.text.TextWatcher
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.NavHostFragment
+import com.example.trabajofinal2024.databinding.FragmentPucheroBinding
+
+class PucheroFragment : Fragment() {
+
+    private lateinit var binding: FragmentPucheroBinding
+
+    private val encuestaViewModel: EncuestaViewModel by viewModels() {
+        EncuestaViewModelFactory((activity?.application as EncuestaApp).repositorio)
+    }
+
+    val pucheroViewModel: PucheroViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_puchero, container, false)
+
+        binding.pucheroviewmodel = pucheroViewModel
+        binding.lifecycleOwner = this
+
+        setupClickListeners(binding, encuestaViewModel)
+
+
+        return binding.root
+
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        super.onViewCreated(view, savedInstanceState)
+        configurarNumeroVeces()
+        configurarSpinner()
+        pucheroViewModel.frecuencia.observe(viewLifecycleOwner, Observer {
+            actualizarRadioGroup()
+        })
+        configurarRadioGroup()
+
+        pucheroViewModel.cantidad.observe(viewLifecycleOwner, Observer {
+            binding.spinnerOpciones.setSelection(pucheroViewModel.cantidadList.indexOf(it))
+        })
+
+        binding.increment.setOnClickListener {
+            val valorActual = binding.numeroid.text.toString().toIntOrNull() ?: 0
+            val nuevoValor = valorActual + 1
+            binding.numeroid.setText(nuevoValor.toString())
+        }
+
+        binding.decrement.setOnClickListener {
+            val valorActual = binding.numeroid.text.toString().toIntOrNull() ?: 0
+            if (valorActual > 0) {
+                val nuevoValor = valorActual - 1
+                binding.numeroid.setText(nuevoValor.toString())
+            }
+        }
+
+        binding.numeroid.filters = arrayOf(InputFilter { source, start, end, dest, dstart, dend ->
+            if (source.all { it.isDigit() }) {
+                null // Permitir la entrada
+            } else {
+                "" // No permitir la entrada (eliminar el texto ingresado)
+            }
+        })
+
+    }
+
+    fun actualizarRadioGroup() {
+        val frecuenciaSeleccionada =pucheroViewModel.frecuencia.value ?: ""
+        when(frecuenciaSeleccionada) {
+            "Diaria" -> binding.frecuenciaGroup.check(R.id.diarioId)
+            "Semanal" -> binding.frecuenciaGroup.check(R.id.semanalId)
+            "Mensual" -> binding.frecuenciaGroup.check(R.id.mensualId)
+            "Anual" -> binding.frecuenciaGroup.check(R.id.anualId)
+            "Nunca" -> binding.frecuenciaGroup.check(R.id.nuncaId)
+        }
+    }
+
+    fun configurarNumeroVeces() {
+        binding.numeroid.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                Log.d("TextWatcher", "BeforeTextChanged producido")
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                Log.d("TextWatcher", "onTextChanged lanzado")
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                s?.let {
+                    pucheroViewModel.setNumeroVeces(s.toString())
+                }
+            }
+        })
+    }
+
+    fun configurarRadioGroup() {
+        binding.frecuenciaGroup.setOnCheckedChangeListener { _, checkedId ->
+            val frecuencia = when(checkedId) {
+                R.id.diarioId -> "Diaria"
+                R.id.semanalId -> "Semanal"
+                R.id.mensualId -> "Mensual"
+                R.id.anualId -> "Anual"
+                R.id.nuncaId -> "Nunca"
+                else -> ""
+            }
+            pucheroViewModel.setFrecuencia(frecuencia)
+        }
+    }
+
+    fun configurarSpinner() {
+        val spinner: Spinner = binding.spinnerOpciones
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, pucheroViewModel.cantidadList)
+        spinner.adapter = adapter
+
+        spinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                parent?.let {
+                    val valorSeleccionado = parent.getItemAtPosition(position).toString()
+                    pucheroViewModel.setCantidad(valorSeleccionado)
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+        }
+    }
+
+    private fun setupClickListeners(binding: FragmentPucheroBinding, viewModel: EncuestaViewModel){
+        binding.enviar.setOnClickListener{
+            viewModel.insert(
+                Encuesta(
+                    nombre_alimento = pucheroViewModel.alimento.value ?:"",
+                    cantidad_alimento = pucheroViewModel.cantidad.value ?:"",
+                    numero_veces = pucheroViewModel.numeroveces.value ?:"",
+                    frecuencia_veces = pucheroViewModel.frecuencia.value ?:""
+                )
+            )
+            Toast.makeText(context, "Encuesta completada", Toast.LENGTH_SHORT).show()
+            NavHostFragment.findNavController(this).navigate(R.id.action_pucheroFragment_to_welcomeLogin2)
+
+        }
+    }
+
+}
